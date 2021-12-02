93dd68c285d083b3a1b982e3ce0ea54e0d0a6f41
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2021-12-02 20:44:56 +0700

    upload

--- resources/views/partials_dashboard/home.blade.php
+++ resources/views/partials_dashboard/home.blade.php
@@ -61,7 +61,13 @@
 
                     <p>Unique Visitors</p>
                   </div>
-                 
+                  <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                  </div>
+                  <a href="#" class="small-box-footer"
+                    >More info <i class="fas fa-arrow-circle-right"></i
+                  ></a>
+                </div>
               </div>
               <!-- ./col -->
             </div>
